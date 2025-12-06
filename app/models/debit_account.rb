288aa59804a0b37ef018394421b79817760e1aab
Joyvis Santana <jjs276@gmail.com>
2025-12-06 20:23:55 -0300

    feat(payment-methods): include debit account sti model

---
+++ app/models/debit_account.rb
@@ -0,0 +1,13 @@
+class DebitAccount < PaymentMethod
+end
+
+# == Schema Information
+#
+# Table name: payment_methods
+#
+#  id         :uuid             not null, primary key
+#  name       :string           not null
+#  type       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
--- db/migrate/20251206230544_create_payment_methods.rb
+++ db/migrate/20251206230544_create_payment_methods.rb
@@ -6,5 +6,7 @@ class CreatePaymentMethods < ActiveRecord::Migration[8.0]
 
       t.timestamps
     end
+
+    PaymentMethod.create(name: 'Default', type: 'DebitAccount')
   end
 end
