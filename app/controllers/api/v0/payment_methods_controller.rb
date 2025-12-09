a70a4e439ff839aa37e8a2d27eddc32c00f70b4d
Joyvis Santana <jjs276@gmail.com>
2025-12-09 15:43:40 -0300

    feat(payment-methods): create transaction to sync balance for debit account

--- app/controllers/api/v0/payment_methods_controller.rb
+++ app/controllers/api/v0/payment_methods_controller.rb
@@ -1,8 +1,17 @@
 class Api::V0::PaymentMethodsController < ApplicationController
   def create
+    # persist payment method
     attributes = payment_method_params
-    attributes[:balance] = 0
+    attributes[:balance] = params[:payment_method][:initial_balance]
     payment_method = DebitAccount.create!(attributes)
+
+    # create initial balance transaction
+    Income.create!(
+      amount: params[:payment_method][:initial_balance],
+      due_date: Date.current,
+      description: 'Initial balance',
+      payment_method_id: payment_method.id
+    )
 
     render json: payment_method, status: :created
   end
--- spec/requests/api/v0/payment_methods_spec.rb
+++ spec/requests/api/v0/payment_methods_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V0::PaymentMethods", type: :request do
+  let(:parsed_response) { JSON.parse(response.body, symbolize_names: true) }
+
   describe 'POST /api/v0/payment_methods' do
     before do
       post '/api/v0/payment_methods', params: { payment_method: params }
@@ -11,12 +13,16 @@ RSpec.describe "Api::V0::PaymentMethods", type: :request do
         let(:params) do
           { name: 'test', type: 'DebitAccount', initial_balance: 50 }
         end
+        let(:income_transaction) { Income.first }
 
         # consider negative balance - Expense transaction
         it 'creates a payment method' do
           expect(response).to have_http_status(:created)
           expect(DebitAccount.count).to eq(1)
-          # expect(Income.count).to eq(1)
+          expect(parsed_response[:balance]).to eq(50)
+          expect(Income.count).to eq(1)
+          expect(income_transaction.amount).to eq(50)
+          expect(income_transaction.description).to eq('Initial balance')
         end
       end
     end
