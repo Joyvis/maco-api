2bb5cb94e7ce47136e22b7df4ffbebc981810ac9
Joyvis Santana <jjs276@gmail.com>
2025-12-24 11:00:39 -0300

    fix(payment-methods): remove default value for transactions

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,3 +3,26 @@ class Invoice < Transaction
 
   has_many :invoice_items, class_name: "Expense", foreign_key: :invoice_id
 end
+
+# == Schema Information
+#
+# Table name: transactions
+#
+#  id                :uuid             not null, primary key
+#  amount            :decimal(, )      not null
+#  type              :string           not null
+#  due_date          :date             not null
+#  description       :text
+#  category_id       :uuid
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  payment_method_id :uuid             not null
+#  paid_at           :datetime
+#  invoice_id        :uuid
+#
+# Indexes
+#
+#  index_transactions_on_category_id        (category_id)
+#  index_transactions_on_invoice_id         (invoice_id)
+#  index_transactions_on_payment_method_id  (payment_method_id)
+#
