785b7674376b5cd9fde8463477c5052a25b01e33
Joyvis Santana <jjs276@gmail.com>
2025-12-27 13:21:50 -0300

    MACO-34 feat(transactions): include paid at to the expense and invoice transactions

--- app/serializers/invoice_serializer.rb
+++ app/serializers/invoice_serializer.rb
@@ -1,5 +1,5 @@
 class InvoiceSerializer < ActiveModel::Serializer
-  attributes :id, :amount, :description, :due_date, :status, :type
+  attributes :id, :amount, :description, :due_date, :status, :type, :paid_at
 
   has_many :invoice_items
 end
