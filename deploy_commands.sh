4ba99eb8938d4d6f43c135aa1362c331b5b57b27
Joyvis Santana <jjs276@gmail.com>
2026-01-02 13:10:14 -0300

    ci(deploy): include migrate command to script

--- deploy_commands.sh
+++ deploy_commands.sh
@@ -1,6 +1,5 @@
+lsof -ti:3000 | xargs kill
 if [ -f /tmp/pids/server.pid ]; then
-  PID=$(cat /tmp/pids/server.pid)
-  kill -9 $PID
   rm /tmp/pids/server.pid
 fi
 
@@ -8,4 +7,5 @@ git pull
 export RAILS_ENV=production
 export DATABASE_HOST=maco-api-production.c8le0m846q8h.us-east-1.rds.amazonaws.com
 
+bin/rails db:migrate
 bin/rails s -b 0.0.0.0 -p 3000 > /dev/null 2>&1 &
