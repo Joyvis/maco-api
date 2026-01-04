349d5d4466928607a90e0737b0e83d9127123277
Joyvis Santana <jjs276@gmail.com>
2026-01-04 13:14:18 -0300

    MACO-57 feat(transaction-creator): implement full cycle of transaction creation

--- app/services/transaction_creator.rb
+++ app/services/transaction_creator.rb
@@ -1,2 +1,16 @@
 class TransactionCreator
+  class InvalidParams < StandardError; end
+
+  attr_reader :repo
+
+  def initialize(repo:)
+    @repo = repo
+  end
+
+  def call(params:)
+    repo.create(params)
+  rescue ActiveRecord::RecordInvalid
+    # TODO: implement logs
+    raise InvalidParams
+  end
 end
--- spec/services/transaction_creator_spec.rb
+++ spec/services/transaction_creator_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe TransactionCreator do
-  subject(:transaction) { described_class.new }
+  let(:params) { { amount: 100 } }
+  subject { described_class.new(repo: repo).call(params: params) }
 
-  it 'returns an instance of TransactionCreator' do
-    expect(transaction).to be_a(TransactionCreator)
+  context 'when params are valid' do
+    let(:repo) { double(:repo, create: transaction) }
+    let(:transaction) { double(:transaction) }
+
+    it 'creates a transaction' do
+      expect(subject).to eq(transaction)
+      expect(repo).to have_received(:create).with(params)
+    end
+  end
+
+  context 'when params are invalid' do
+    before { allow(repo).to receive(:create).and_raise(ActiveRecord::RecordInvalid) }
+    let(:repo) { double(:repo, create: nil) }
+
+    it 'raises an error' do
+      expect { subject }.to raise_error(TransactionCreator::InvalidParams)
+    end
   end
 end
