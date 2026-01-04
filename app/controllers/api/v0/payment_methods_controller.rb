179258934e782c1564dbfed794c9a769e18b0f41
Joyvis Santana <jjs276@gmail.com>
2026-01-04 14:01:23 -0300

    MACO-57 refact(transactions): remove duplicated code when creating payment methods

--- app/controllers/api/v0/payment_methods_controller.rb
+++ app/controllers/api/v0/payment_methods_controller.rb
@@ -11,15 +11,37 @@ class Api::V0::PaymentMethodsController < ApplicationController
 
     if params[:payment_method][:initial_balance] &&
         !params[:payment_method][:initial_balance].to_f.zero?
-      Income.create!(
-        amount: params[:payment_method][:initial_balance],
-        due_date: Date.current,
-        description: "Initial balance",
-        payment_method_id: payment_method.id
-      )
+
+      repo = TransactionsRepository.new(type: transaction_type)
+      transaction_params = transaction_params(payment_method)
+      transaction_params[:category_id] = create_category if transaction_type == Expense
+      TransactionCreator.
+        new(repo: repo).
+        call(params: transaction_params)
     end
 
     render json: payment_method, status: :created
+  end
+
+  def create_category
+    Category.create!(name: "Unexpected Expenses").id
+  end
+
+  def transaction_params(payment_method)
+    {
+      amount: params[:payment_method][:initial_balance],
+      due_date: Date.current,
+      description: "Initial balance",
+      payment_method_id: payment_method.id
+    }
+  end
+
+  def transaction_type
+    return Expense if payment_method_klass == CreditAccount
+
+    return Income if params[:payment_method][:initial_balance].to_f.positive?
+
+    return Expense
   end
 
   def build_transaction
